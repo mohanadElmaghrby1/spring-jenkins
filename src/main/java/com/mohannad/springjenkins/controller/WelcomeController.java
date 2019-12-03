305ac0a227487b77d60c581f2e232c27918aac1e
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-12-03 13:54:26 +0200

    initial commit

---
+++ src/main/java/com/mohannad/springjenkins/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.mohannad.springjenkins.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello static";
+    }
+}
